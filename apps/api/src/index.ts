ccded6176c54eff78d6b7b8aa85948cd1273910c
LewieJ08 <lewiej08@gmail.com>
2026-03-03 14:35:39 +0000

    Add health api endpoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import healthRoutes from './routes/health'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route('/api/health', healthRoutes)
 
 export default app
